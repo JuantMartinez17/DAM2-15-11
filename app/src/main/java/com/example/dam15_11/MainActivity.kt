e9d90e9bb58017774ae4ff0a9f10885a476cfaec
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-15 19:28:33 -0300

    init project

---
+++ app/src/main/java/com/example/dam15_11/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.dam15_11
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dam15_11.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
